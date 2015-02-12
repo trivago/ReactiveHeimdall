16f8d80a84c6f397b6e0eb80b69a9648faebfcfa
Tim Brückmann <tim@tibr.me>
2015-02-12 09:13:04 +0100

    Create Xcode project

---
+++ ReactiveHeimdall/ReactiveHeimdall.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveHeimdall.h
+//  ReactiveHeimdall
+//
+//  Created by Tim Brückmann on 12.02.15.
+//  Copyright (c) 2015 Rheinfabrik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveHeimdall.
+FOUNDATION_EXPORT double ReactiveHeimdallVersionNumber;
+
+//! Project version string for ReactiveHeimdall.
+FOUNDATION_EXPORT const unsigned char ReactiveHeimdallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveHeimdall/PublicHeader.h>
+
+
