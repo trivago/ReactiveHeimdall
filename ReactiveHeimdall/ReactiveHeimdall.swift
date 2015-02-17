107544587002e9b8024d23f883f1eed96552190e
Tim Brückmann <tim@tibr.me>
2015-02-17 17:13:48 +0100

    Add documentation

--- ReactiveHeimdall/ReactiveHeimdall.swift
+++ ReactiveHeimdall/ReactiveHeimdall.swift
@@ -4,6 +4,13 @@ import ReactiveCocoa
 
 extension Heimdall {
     
+    /// Requests an access token with the resource owner's password credentials.
+    ///
+    /// :param: username The resource owner's username.
+    /// :param: password The resource owner's password.
+    ///
+    /// :returns: A signal that completes when the requests finishes successfully
+    ///     or sends an error of the requests finishes with an error.
     public func requestAccessToken(username: String, password: String) -> RACSignal {
         return RACSignal.createSignal { subscriber in
             self.requestAccessToken(username: username, password: password) { result in
@@ -18,6 +25,18 @@ extension Heimdall {
         }
     }
     
+    /// Alters the given request by adding authentication, if possible.
+    ///
+    /// In case of an expired access token and the presence of a refresh token,
+    /// automatically tries to refresh the access token.
+    ///
+    /// **Note:** If the access token must be refreshed, network I/O is
+    ///     performed.
+    ///
+    /// :param: request An unauthenticated NSURLRequest.
+    ///
+    /// :returns: A signal that sends the authenticated request on success or
+    ///     an error when the request could not be authenticated.
     public func authenticateRequest(request: NSURLRequest) -> RACSignal {
         return RACSignal.createSignal { subscriber in
             self.authenticateRequest(request) { result in
