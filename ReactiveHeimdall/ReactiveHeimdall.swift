69e41c94aaf7698b58a39852f3a1797d86723ca8
Tim Brückmann <tim@tibr.me>
2015-03-09 12:50:43 +0100

    Add @objc annotations

--- ReactiveHeimdall/ReactiveHeimdall.swift
+++ ReactiveHeimdall/ReactiveHeimdall.swift
@@ -12,6 +12,7 @@ extension Heimdall {
     /// :returns: A signal that sends a `RACUnit` and completes when the
     ///     request finishes successfully or sends an error of the request
     ///     finishes with an error.
+    @objc
     public func requestAccessToken(username: String, password: String) -> RACSignal {
         return RACSignal.createSignal { subscriber in
             self.requestAccessToken(username: username, password: password) { result in
@@ -39,6 +40,7 @@ extension Heimdall {
     ///
     /// :returns: A signal that sends the authenticated request on success or
     ///     an error when the request could not be authenticated.
+    @objc
     public func authenticateRequest(request: NSURLRequest) -> RACSignal {
         return RACSignal.createSignal { subscriber in
             self.authenticateRequest(request) { result in
